ee27bedbbdd7c4724d2d974ef6c5d04289c06237
Ragul-Dhandapani <61295079+Ragul-Dhandapani@users.noreply.github.com>
2021-01-31 22:16:39 +0000

    Update Runner.java

--- src/main/java/com/cucumber/allure/integration/runner/Runner.java
+++ src/main/java/com/cucumber/allure/integration/runner/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features="src/main/resources/features/friday.feature",
         plugin = { "pretty", "html:target/cucumber-html-reports", "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
                 "json:target/cucumber-html-reports/cucumber.json","rerun:target/failed_scenarios.txt" },
-        extraGlue ={"com.cybg.cucumber"})
+        extraGlue ={"com.diff.cucumber"})
 public class Runner{
 
 }
